5e03ceaf5a31d3ac35514ccf3960b509c1dc8810
Diego Sano <diego.sano@nave.rs>
2023-02-16 14:55:57 -0300

    feat: categories module

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 
 const ContactController = require('./app/controllers/contact-controller');
+const CategoryController = require('./app/controllers/category-controller');
 
 const router = Router();
 
@@ -9,5 +10,8 @@ router.get('/contacts/:id', ContactController.show);
 router.delete('/contacts/:id', ContactController.delete);
 router.post('/contacts', ContactController.store);
 router.put('/contacts/:id', ContactController.update);
+
+router.get('/categories', CategoryController.index);
+router.post('/categories', CategoryController.store);
 
 module.exports = router;
