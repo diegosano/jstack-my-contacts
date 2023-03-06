402e1afb094fc60895e11a667110954c4ccdf42d
Diego Sano <diegocsano@outlook.com>
2023-03-06 17:21:22 -0300

    feat: logic to handle multiple origins

--- src/app/middlewares/cors.js
+++ src/app/middlewares/cors.js
@@ -1,11 +1,21 @@
-module.exports = (_request, response, next) => {
-  response.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  response.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, DELETE, PATCH, PUT',
-  );
-  response.setHeader('Access-Control-Allow-Headers', '*');
-  response.setHeader('Access-Control-Max-Age', '10');
+const ALLOWED_ORIGINS = [
+  'http://localhost:3000',
+  'http://localhost:3002',
+];
+
+module.exports = (request, response, next) => {
+  const origin = request.header('origin');
+  const isAllowed = ALLOWED_ORIGINS.includes(origin);
+
+  if (isAllowed) {
+    response.setHeader('Access-Control-Allow-Origin', origin);
+    response.setHeader(
+      'Access-Control-Allow-Methods',
+      'GET, POST, DELETE, PATCH, PUT',
+    );
+    response.setHeader('Access-Control-Allow-Headers', '*');
+    response.setHeader('Access-Control-Max-Age', '10');
+  }
 
   next();
 };
